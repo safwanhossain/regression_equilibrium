c238ffa09d33b5236b88668d77c7f86f9d7c1d22
shossain <shossain@vws77.vector.local>
2019-05-08 14:42:39 -0400

    Analysis and benchmarking

--- main_parallel.py
+++ main_parallel.py
@@ -3,13 +3,14 @@ from best_response_algo import best_response_algo
 from p_regression import generate_data, fit_model
 import matplotlib.pyplot as plt
 import csv
+
 import time
-from tqdm import tqdm
+import concurrent.futures 
 
 ## default values ##
 P = 2
 N = 100
-NUM_SIMS = 100
+NUM_SIMS = 1000
 ALPHA = 1
 XDIM = 1
 
@@ -42,8 +43,34 @@ def plot(x_vals, xlabel, name, br_time_vals, sc_vals, sc_l1_vals=None, log=False
     plt.grid(True)
     
     plt.tight_layout()
-    plt.savefig(name)
-    #plt.show()
+    #plt.savefig(name)
+    plt.show()
+    
+def run_simulation(tup):
+    X, true_Y, n_, with_l1, p = tup
+    proj_Y = np.matmul(X, fit_model(X, true_Y))
+    MSE_h = np.sum(np.power(proj_Y - true_Y, 2))
+
+    if with_l1:
+        proj_Y_l1 = np.matmul(X, fit_model(X, true_Y, p=1))
+        MSE_l1 = np.sum(np.power(proj_Y_l1 - true_Y, 2))
+    else:
+        MSE_l1 = 1
+
+    equi_exists, counter, equi_report, beta_equi = best_response_algo(X, true_Y, p)
+    social_cost, social_cost_l1, br_iters = 1, 1, 1
+    if equi_exists:
+        proj = np.matmul(X, beta_equi)
+        MSE_eq = np.sum(np.power(proj - true_Y, 2))
+     
+        if MSE_eq != 0 and MSE_h != 0:
+            social_cost = MSE_eq/MSE_h
+            social_cost_l1 = MSE_l1/MSE_h
+        br_iters = counter*n_
+    else:
+        print("NO EQUILIBRIUM")
+        #fail_equilibrium_writer.writerow([str(X), str(true_Y)])
+    return (br_iters, social_cost, social_cost_l1)
 
 def sweep_n(n_vals, with_l1=False, to_plot=True):
     csv_file = open("sweep_n.csv", mode='w')
@@ -58,32 +85,24 @@ def sweep_n(n_vals, with_l1=False, to_plot=True):
         social_cost_, social_cost_l1_, br_iters_ = [], [], []
         n_ = int(n_)
         print("n = ", n_)
-        for i in tqdm(range(NUM_SIMS)):
+
+        inputs = []
+        for i in range(NUM_SIMS):
             X, true_Y = generate_data(d=XDIM, n=n_)
-            proj_Y = np.matmul(X, fit_model(X, true_Y))
-            MSE_h = np.sum(np.power(proj_Y - true_Y, 2))
+            tup = (X, true_Y, n_, with_l1, P)
+            inputs.append(tup)
+            
+        executor = concurrent.futures.ProcessPoolExecutor(12)
+        futures = [executor.submit(run_simulation, item) for item in inputs]
+        concurrent.futures.wait(futures)
 
-            if with_l1:
-                proj_Y_l1 = np.matmul(X, fit_model(X, true_Y, p=1))
-                MSE_l1 = np.sum(np.power(proj_Y_l1 - true_Y, 2))
-            else:
-                MSE_l1 = 1
-
-            equi_exists, counter, equi_report, beta_equi = best_response_algo(X, true_Y, P)
-            if equi_exists:
-                proj = np.matmul(X, beta_equi)
-                MSE_eq = np.sum(np.power(proj - true_Y, 2))
-             
-                if MSE_eq == 0 and MSE_h == 0:
-                    social_cost_.append(1)
-                    social_cost_l1_.append(1)
-                else:
-                    social_cost_.append(MSE_eq/MSE_h)
-                    social_cost_l1_.append(MSE_l1/MSE_h)
-                br_iters_.append(counter*n_)
-            else:
-                print("NO EQUILIBRIUM")
-                fail_equilibrium_writer.writerow([str(X), str(true_Y)])
+        for future in futures:
+            assert(future.done())
+            tup = future.result()
+            br_time, sc, sc_l1 = tup
+            br_iters_.append(br_time)
+            social_cost_.append(sc)
+            social_cost_l1_.append(sc_l1)
 
         avg_br = sum(br_iters_)/len(br_iters_)
         avg_sc = sum(social_cost_)/len(social_cost_)
@@ -114,33 +133,25 @@ def sweep_p(p_vals, with_l1=False, to_plot=True):
     for p_ in p_vals:
         social_cost_, social_cost_l1_, br_iters_ = [], [], []
         print("p = ", p_)
-        for i in tqdm(range(NUM_SIMS)):
+        inputs = []
+        
+        for i in range(NUM_SIMS):
             X, true_Y = generate_data(d=XDIM, n=N)
-            proj_Y = np.matmul(X, fit_model(X, true_Y, p=2))
-            MSE_h = np.sum(np.power(proj_Y - true_Y, 2))
+            tup = (X, true_Y, N, with_l1, p_)
+            inputs.append(tup)
+            
+        executor = concurrent.futures.ProcessPoolExecutor(12)
+        futures = [executor.submit(run_simulation, item) for item in inputs]
+        concurrent.futures.wait(futures)
 
-            if with_l1:
-                proj_Y_l1 = np.matmul(X, fit_model(X, true_Y, p=1))
-                MSE_l1 = np.sum(np.power(proj_Y_l1 - true_Y, 2))
-            else:
-                MSE_l1 = 1
-
-            equi_exists, counter, equi_report, beta_equi = best_response_algo(X, true_Y, p_, test=True)
-            if equi_exists:
-                proj = np.matmul(X, beta_equi)
-                MSE_eq = np.sum(np.power(proj - true_Y, 2))
-             
-                if MSE_eq == 0 and MSE_h == 0:
-                    social_cost_.append(1)
-                    social_cost_l1_.append(1)
-                else:
-                    social_cost_.append(MSE_eq/MSE_h)
-                    social_cost_l1_.append(MSE_l1/MSE_h)
-                br_iters_.append(counter*N)
-            else:
-                print("NO EQUILIBRIUM")
-                fail_equilibrium_writer.writerow([str(X), str(true_Y)])
-
+        for future in futures:
+            assert(future.done())
+            tup = future.result()
+            br_time, sc, sc_l1 = tup
+            br_iters_.append(br_time)
+            social_cost_.append(sc)
+            social_cost_l1_.append(sc_l1)
+        
         avg_br = sum(br_iters_)/len(br_iters_)
         avg_sc = sum(social_cost_)/len(social_cost_)
         avg_sc_l1 = sum(social_cost_l1_)/len(social_cost_l1_)
@@ -170,32 +181,24 @@ def sweep_d(d_vals, with_l1=False, to_plot=True):
     for d_ in d_vals:
         social_cost_, social_cost_l1_, br_iters_ = [], [], []
         print("d = ", d_)
-        for i in tqdm(range(NUM_SIMS)):
+        inputs = []
+        
+        for i in range(NUM_SIMS):
             X, true_Y = generate_data(d=d_, n=N)
-            proj_Y = np.matmul(X, fit_model(X, true_Y, p=2))
-            MSE_h = np.sum(np.power(proj_Y - true_Y, 2))
+            tup = (X, true_Y, N, with_l1, P)
+            inputs.append(tup)
+            
+        executor = concurrent.futures.ProcessPoolExecutor(12)
+        futures = [executor.submit(run_simulation, item) for item in inputs]
+        concurrent.futures.wait(futures)
 
-            if with_l1:
-                proj_Y_l1 = np.matmul(X, fit_model(X, true_Y, p=1))
-                MSE_l1 = np.sum(np.power(proj_Y_l1 - true_Y, 2))
-            else:
-                MSE_l1 = 1
-
-            equi_exists, counter, equi_report, beta_equi = best_response_algo(X, true_Y, P, test=True)
-            if equi_exists:
-                proj = np.matmul(X, beta_equi)
-                MSE_eq = np.sum(np.power(proj - true_Y, 2))
-             
-                if MSE_eq == 0 and MSE_h == 0:
-                    social_cost_.append(1)
-                    social_cost_l1_.append(1)
-                else:
-                    social_cost_.append(MSE_eq/MSE_h)
-                    social_cost_l1_.append(MSE_l1/MSE_h)
-                br_iters_.append(counter*N)
-            else:
-                print("NO EQUILIBRIUM")
-                fail_equilibrium_writer.writerow([str(X), str(true_Y)])
+        for future in futures:
+            assert(future.done())
+            tup = future.result()
+            br_time, sc, sc_l1 = tup
+            br_iters_.append(br_time)
+            social_cost_.append(sc)
+            social_cost_l1_.append(sc_l1)
 
         avg_br = sum(br_iters_)/len(br_iters_)
         avg_sc = sum(social_cost_)/len(social_cost_)
@@ -207,7 +210,7 @@ def sweep_d(d_vals, with_l1=False, to_plot=True):
         social_cost.append(avg_sc)
         social_cost_l1.append(avg_sc_l1)
         equi_found.append( len(social_cost_) / NUM_SIMS )
-
+    
     if to_plot:
         if with_l1 == True:
             plot(d_vals, "X dimension", "sweep_d", br_iters, social_cost, social_cost_l1)
@@ -227,13 +230,6 @@ def benchmark_d():
     end = time.time()
     print(end-start)
 
-def benchmark_p():
-    print("Benchmarking p")
-    start = time.time()
-    sweep_p([1.2, 3, 5], with_l1=True, to_plot=False)
-    end = time.time()
-    print(end-start)
-
 if __name__ == "__main__":
     #n_vals = np.concatenate([np.arange(10,100,10), np.arange(100,1050,50), np.arange(1000, 11000, 1000)])
     #sweep_n(n_vals, with_l1=True)
@@ -244,4 +240,5 @@ if __name__ == "__main__":
     #p_vals = [1.2, 1.4, 1.6, 1.8, 2, 4, 6, 8, 10, 100]
     #sweep_p(p_vals, with_l1=True)
     
-    benchmark_p() 
+    benchmark_d()
+
