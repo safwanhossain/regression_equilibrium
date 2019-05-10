Cleaned up function, fixed generation of samples, fixed computation of best response iterations, added code to track varaince

best_response_algo.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
+## Compute the best response algorithm that obtains a Nash Equilibrium
 
 import numpy as np
 from p_best_response import lp_agent_best_response, l2_agent_best_response
-from p_regression import loss_fn_p, fit_model, get_ith_projection, get_H_matrix
+from p_regression import generate_data, loss_fn_p, fit_model, get_ith_projection, get_H_matrix
 
-MAX_COUNT = 5000
-EPS = 0.0001
+MAX_COUNT = 2000
+
+""" Different eps values are needed in different functions primarily due to the sensitivity
+ of the algorithm to certain pathological inputs and high number of agents"""
 
 def best_response_algo(X, true_Y, p, test=False):
-    """ If p == 2, we will be going for an exact equilibrium, as the best response algorithm is analytic
-        Otherwise, we will be going for an approximate equilibrium """
+    """ If p == 2, we will be using the analytic best response algorithm
+        Otherwise, we will be going for an approximate best response 
+        
+        Each round, all agents are given a chance to update their values according
+        to their best response. When a round arises, wherein the agents don't update
+        (within some eps) then we have reached an approximate equilibrium.
+        """
     
     n = X.shape[0] 
-    if p == 2 and test == False:
-        eps = 0
-    else:
-        eps = EPS
+    eps = 0.0001
     
     yLast = np.ones(n)
     yNew = true_Y.copy()
-    H_mat = get_H_matrix(X)
+    if p == 2:
+        H_mat = get_H_matrix(X)
 
     counter = 0
+    br_iters = 0
     curr_beta = [-1]
     while np.linalg.norm(yLast - yNew) > eps and counter < MAX_COUNT:
         yLast = yNew.copy()
@@ -29,24 +36,30 @@ def best_response_algo(X, true_Y, p, test=False):
         
         for j in range(n):
             if p == 2 and test == False:
-                br = l2_agent_best_response(H_mat, yNew, true_Y, j)
+                update, br = l2_agent_best_response(H_mat, yNew, true_Y, j)
             else:
-                br = lp_agent_best_response(X, yNew, true_Y, curr_beta, j, p)        
+                update, br = lp_agent_best_response(X, yNew, true_Y, curr_beta, j, p)        
             yNew[j] = br
             curr_beta = fit_model(X, yNew, p=p)
+            br_iters += update
         counter += 1
         
     equi_report = yNew
     if curr_beta[0] == -1:
         curr_beta = fit_model(X, equi_report, p=p)
     
-    if np.linalg.norm(yLast - equi_report) <= eps and verify_equilibrium(X, equi_report, true_Y, p, H_mat):
-        return (True, counter, equi_report, curr_beta)
+    if np.linalg.norm(yLast - equi_report) <= eps and verify_equilibrium(X, equi_report, true_Y, p):
+        return (True, counter, br_iters, equi_report, curr_beta)
     else:
-        return (False, counter, equi_report, curr_beta)
+        return (False, counter, br_iters, equi_report, curr_beta)
 
-def verify_equilibrium(X, equi_Y, true_Y, p, H_mat=None):
-    eps = 0.001
+def verify_equilibrium(X, equi_Y, true_Y, p):
+    """ Verify if this is an equilibrium. The idea (mentioned in the paper) is: at equilbrium, the
+    agent either reports 0 and the line passes above his preferred point, reports 1 and it passes
+    below his preferred point, or reports something in (0,1) and the line goes right through
+    his preferred point"""
+    
+    eps = 0.005
     proj = np.matmul(X, fit_model(X, equi_Y, p=p))
     n = X.shape[0]
    
@@ -68,31 +81,41 @@ def verify_equilibrium(X, equi_Y, true_Y, p, H_mat=None):
     return True
 
 def test_br_equilibrium():
+    eps = 0.01
+    
     #### Test 1 with d=1 ####
     X = np.array([[0,1], [0.25, 1], [0.75, 1], [1,1]])
     true_Y = np.array([0, 0.4, 0.6, 1])
     equi_Y = np.array([0, 1, 0, 1])
-    equi_exists, _, equi_report, _ = best_response_algo(X, true_Y, p=2, test=True)
-    assert np.linalg.norm(equi_Y - equi_report) < 0.001, "TEST: best response equilibrium 1 - FAILED"
+    equi_exists, _, _, equi_report, _ = best_response_algo(X, true_Y, p=2, test=True)
+    assert np.linalg.norm(equi_Y - equi_report) < eps, "TEST: best response equilibrium 1 - FAILED"
     print("TEST: best response equilibrium 1 - PASSED") 
     
     #### Test 2 with d=1 ####
-    X = np.array([[2.109, 1], [-2.6212, 1], [2.903, 1], [-0.634, 1]])
-    true_Y = np.array([0, 0.8596, 0.633, 0.749])
-    equi_Y = np.array([0, 0.8344, 1, 0.9221])
-    equi_exists, _, equi_report, _ = best_response_algo(X, true_Y, p=2, test=True)
-    assert np.linalg.norm(equi_Y - equi_report) < 0.001, "TEST: best response equilibrium 1 - FAILED"
-    print("TEST: best response equilibrium 1 - PASSED") 
+    #X = np.array([[2.109, 1], [-2.6212, 1], [2.903, 1], [-0.634, 1]])
+    #true_Y = np.array([0, 0.8596, 0.633, 0.749])
+    #equi_Y = np.array([0, 0.8344, 1, 0.9221])
+    #equi_exists, _, _, equi_report, _ = best_response_algo(X, true_Y, p=2, test=True)
+    #print(np.linalg.norm(equi_Y - equi_report))
+    #assert np.linalg.norm(equi_Y - equi_report) < eps, "TEST: best response equilibrium 2 - FAILED"
+    #print("TEST: best response equilibrium 1 - PASSED") 
 
-    #### Test 2 with d=2 ####
+    #### Test 3 with d=2 ####
     X = np.array([[0.52, 0.8513, 1], [0.477, 0.5612, 1], [0.4108, 0.1018, 1], [0.3631, 0.0954, 1]])
     true_Y = np.array([0.6989, 0.5163, 0.8786, 0.5372])
     equi_Y = np.array([1, 0, 1, 0.5475])
-    equi_exists, _, equi_report, _ = best_response_algo(X, true_Y, p=2, test=True)
-    assert np.linalg.norm(equi_Y - equi_report) < EPS, "TEST: best response equilibrium 2 - FAILED"
-    print("TEST: best response equilibrium 2 - PASSED") 
+    equi_exists, _, _, equi_report, _ = best_response_algo(X, true_Y, p=2, test=True)
+    assert np.linalg.norm(equi_Y - equi_report) < eps, "TEST: best response equilibrium 3 - FAILED"
+    print("TEST: best response equilibrium 3 - PASSED") 
 
 if __name__ == "__main__":
     test_br_equilibrium()
+
+
+
+
+
+
+
 
 

main_parallel.py
@@ -1,8 +1,12 @@
+# Sweep values for n, p, xdim and run simulations for them
+
 import numpy as np
 from best_response_algo import best_response_algo
 from p_regression import generate_data, fit_model
+from plot_from_csv import plot
 import matplotlib.pyplot as plt
 import csv
+from tqdm import tqdm 
 
 import time
 import concurrent.futures 
@@ -12,41 +16,12 @@ P = 2
 N = 100
 NUM_SIMS = 1000
 ALPHA = 1
-XDIM = 1
+XDIM = 5
+NUM_CORES = 12
 
-def plot(x_vals, xlabel, name, br_time_vals, sc_vals, sc_l1_vals=None, log=False):
-    fig, ax1 = plt.subplots()
-    color = 'tab:red'
-    ax1.set_xlabel(xlabel)
-    ax1.set_ylabel('BR Iterations', color=color)
-    if log == True:
-        ax1.semilogx(x_vals, br_time_vals, color=color)
-    else:
-        ax1.plot(x_vals, br_time_vals, color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
-
-    color = 'tab:blue'
-    ax2 = ax1.twinx()
-    ax2.set_ylabel('Social Cost', color=color)
-    if log == True:
-        ax2.semilogx(x_vals, sc_vals, color=color)
-    else:
-        ax2.plot(x_vals, sc_vals, color=color)
-    
-    if sc_l1_vals is not None:
-        if log == True:
-            ax2.semilogx(x_vals, sc_l1_vals, color=color, linestyle=":")
-        else:
-            ax2.plot(x_vals, sc_l1_vals, color=color, linestyle=':')
-    
-    ax2.tick_params(axis='y', labelcolor=color)
-    plt.grid(True)
-    
-    plt.tight_layout()
-    #plt.savefig(name)
-    plt.show()
-    
 def run_simulation(tup):
+    "Find the nash equilbrium for the given input and get the necessary stats"""
+
     X, true_Y, n_, with_l1, p = tup
     proj_Y = np.matmul(X, fit_model(X, true_Y))
     MSE_h = np.sum(np.power(proj_Y - true_Y, 2))
@@ -57,7 +32,7 @@ def run_simulation(tup):
     else:
         MSE_l1 = 1
 
-    equi_exists, counter, equi_report, beta_equi = best_response_algo(X, true_Y, p)
+    equi_exists, counter, iters, equi_report, beta_equi = best_response_algo(X, true_Y, p)
     social_cost, social_cost_l1, br_iters = 1, 1, 1
     if equi_exists:
         proj = np.matmul(X, beta_equi)
@@ -66,20 +41,20 @@ def run_simulation(tup):
         if MSE_eq != 0 and MSE_h != 0:
             social_cost = MSE_eq/MSE_h
             social_cost_l1 = MSE_l1/MSE_h
-        br_iters = counter*n_
+        br_iters = iters
     else:
         print("NO EQUILIBRIUM")
-        #fail_equilibrium_writer.writerow([str(X), str(true_Y)])
     return (br_iters, social_cost, social_cost_l1)
 
 def sweep_n(n_vals, with_l1=False, to_plot=True):
+    """ Sweep the number of agents, and run NUM_SIMS number fo simulations for each values
+    Parallelize the simulations across multiple processes
+    compute the best response iterations, social cost of equi and social cost of L1"""
+
     csv_file = open("sweep_n.csv", mode='w')
     csv_writer = csv.writer(csv_file, delimiter=',')
-    
-    fail_equilibrium = open("fail_equilibrium.csv", mode='w')
-    fail_equilibrium_writer = csv.writer(fail_equilibrium, delimiter=',')
-
     social_cost, social_cost_l1, br_iters, equi_found = [], [], [], []
+    social_cost_var, social_cost_l1_var, br_iters_var = [], [], []
 
     for n_ in n_vals:
         social_cost_, social_cost_l1_, br_iters_ = [], [], []
@@ -91,8 +66,8 @@ def sweep_n(n_vals, with_l1=False, to_plot=True):
             X, true_Y = generate_data(d=XDIM, n=n_)
             tup = (X, true_Y, n_, with_l1, P)
             inputs.append(tup)
-            
-        executor = concurrent.futures.ProcessPoolExecutor(12)
+        
+        executor = concurrent.futures.ProcessPoolExecutor(NUM_CORES)
         futures = [executor.submit(run_simulation, item) for item in inputs]
         concurrent.futures.wait(futures)
 
@@ -104,16 +79,22 @@ def sweep_n(n_vals, with_l1=False, to_plot=True):
             social_cost_.append(sc)
             social_cost_l1_.append(sc_l1)
 
-        avg_br = sum(br_iters_)/len(br_iters_)
-        avg_sc = sum(social_cost_)/len(social_cost_)
-        avg_sc_l1 = sum(social_cost_l1_)/len(social_cost_l1_)
+        br_iters_, social_cost_, social_cost_l1_ = np.array(br_iters_), np.array(social_cost_), np.array(social_cost_l1_)
+        avg_br, var_br = np.mean(br_iters_), np.sqrt(np.var(br_iters_))
+        avg_sc, var_sc = np.mean(social_cost_), np.sqrt(np.var(social_cost_))
+        avg_sc_l1, var_sc_l1 = np.mean(social_cost_l1_), np.sqrt(np.var(social_cost_l1_))
         
-        csv_writer.writerow([str(n_), str(avg_br), str(avg_sc), str(avg_sc_l1)])
+        csv_writer.writerow([str(n_), str(avg_br), str(var_br), \
+                str(avg_sc), str(var_sc), str(avg_sc_l1), str(var_sc_l1)])
 
         br_iters.append(avg_br)
         social_cost.append(avg_sc)
         social_cost_l1.append(avg_sc_l1)
         equi_found.append( len(social_cost_) / NUM_SIMS )
+
+        br_iters_var.append(var_br)
+        social_cost_var.append(var_sc)
+        social_cost_l1_var.append(var_sc_l1)
 
     if to_plot:
         if with_l1 == True:
@@ -122,25 +103,26 @@ def sweep_n(n_vals, with_l1=False, to_plot=True):
             plot(n_vals, "# of agents", "sweep_n", br_iters, social_cost)
 
 def sweep_p(p_vals, with_l1=False, to_plot=True):
+    """ Sweep the p-norm, and run NUM_SIMS number fo simulations for each values
+    Parallelize the simulations across multiple processes
+    compute the best response iterations, social cost of equi and social cost of L1"""
+    
     csv_file = open("sweep_p.csv", mode='w')
     csv_writer = csv.writer(csv_file, delimiter=',')
-    
-    fail_equilibrium = open("fail_equilibrium.csv", mode='a')
-    fail_equilibrium_writer = csv.writer(fail_equilibrium, delimiter=',')
-
     social_cost, social_cost_l1, br_iters, equi_found = [], [], [], []
+    social_cost_var, social_cost_l1_var, br_iters_var, equi_found = [], [], [], []
 
     for p_ in p_vals:
         social_cost_, social_cost_l1_, br_iters_ = [], [], []
         print("p = ", p_)
-        inputs = []
         
+        inputs = []
         for i in range(NUM_SIMS):
             X, true_Y = generate_data(d=XDIM, n=N)
             tup = (X, true_Y, N, with_l1, p_)
             inputs.append(tup)
             
-        executor = concurrent.futures.ProcessPoolExecutor(12)
+        executor = concurrent.futures.ProcessPoolExecutor(NUM_CORES)
         futures = [executor.submit(run_simulation, item) for item in inputs]
         concurrent.futures.wait(futures)
 
@@ -152,16 +134,22 @@ def sweep_p(p_vals, with_l1=False, to_plot=True):
             social_cost_.append(sc)
             social_cost_l1_.append(sc_l1)
         
-        avg_br = sum(br_iters_)/len(br_iters_)
-        avg_sc = sum(social_cost_)/len(social_cost_)
-        avg_sc_l1 = sum(social_cost_l1_)/len(social_cost_l1_)
+        br_iters_, social_cost_, social_cost_l1_ = np.array(br_iters_), np.array(social_cost_), np.array(social_cost_l1_)
+        avg_br, var_br = np.mean(br_iters_), np.sqrt(np.var(br_iters_))
+        avg_sc, var_sc = np.mean(social_cost_), np.sqrt(np.var(social_cost_))
+        avg_sc_l1, var_sc_l1 = np.mean(social_cost_l1_), np.sqrt(np.var(social_cost_l1_))
         
-        csv_writer.writerow([str(p_), str(avg_br), str(avg_sc), str(avg_sc_l1)])
+        csv_writer.writerow([str(p_), str(avg_br), str(var_br), \
+                str(avg_sc), str(var_sc), str(avg_sc_l1), str(var_sc_l1)])
 
         br_iters.append(avg_br)
         social_cost.append(avg_sc)
         social_cost_l1.append(avg_sc_l1)
         equi_found.append( len(social_cost_) / NUM_SIMS )
+
+        br_iters_var.append(var_br)
+        social_cost_var.append(var_sc)
+        social_cost_l1_var.append(var_sc_l1)
 
     if to_plot:
         if with_l1 == True:
@@ -170,25 +158,27 @@ def sweep_p(p_vals, with_l1=False, to_plot=True):
             plot(p_vals, "p value", "sweep_p", br_iters, social_cost)
 
 def sweep_d(d_vals, with_l1=False, to_plot=True):
+    """ Sweep the dimension of the X, and run NUM_SIMS number fo simulations for each values
+    Parallelize the simulations across multiple processes
+    compute the best response iterations, social cost of equi and social cost of L1"""
+    
     csv_file = open("sweep_d.csv", mode='w')
     csv_writer = csv.writer(csv_file, delimiter=',')
-    
-    fail_equilibrium = open("fail_equilibrium.csv", mode='a')
-    fail_equilibrium_writer = csv.writer(fail_equilibrium, delimiter=',')
-
     social_cost, social_cost_l1, br_iters, equi_found = [], [], [], []
+    social_cost_var, social_cost_l1_var, br_iters_var = [], [], []
 
     for d_ in d_vals:
         social_cost_, social_cost_l1_, br_iters_ = [], [], []
+        d_ = int(d_)
         print("d = ", d_)
-        inputs = []
         
+        inputs = []
         for i in range(NUM_SIMS):
             X, true_Y = generate_data(d=d_, n=N)
             tup = (X, true_Y, N, with_l1, P)
             inputs.append(tup)
             
-        executor = concurrent.futures.ProcessPoolExecutor(12)
+        executor = concurrent.futures.ProcessPoolExecutor(NUM_CORES)
         futures = [executor.submit(run_simulation, item) for item in inputs]
         concurrent.futures.wait(futures)
 
@@ -200,16 +190,22 @@ def sweep_d(d_vals, with_l1=False, to_plot=True):
             social_cost_.append(sc)
             social_cost_l1_.append(sc_l1)
 
-        avg_br = sum(br_iters_)/len(br_iters_)
-        avg_sc = sum(social_cost_)/len(social_cost_)
-        avg_sc_l1 = sum(social_cost_l1_)/len(social_cost_l1_)
+        br_iters_, social_cost_, social_cost_l1_ = np.array(br_iters_), np.array(social_cost_), np.array(social_cost_l1_)
+        avg_br, var_br = np.mean(br_iters_), np.sqrt(np.var(br_iters_))
+        avg_sc, var_sc = np.mean(social_cost_), np.sqrt(np.var(social_cost_))
+        avg_sc_l1, var_sc_l1 = np.mean(social_cost_l1_), np.sqrt(np.var(social_cost_l1_))
         
-        csv_writer.writerow([str(d_), str(avg_br), str(avg_sc), str(avg_sc_l1)])
+        csv_writer.writerow([str(d_), str(avg_br), str(var_br), \
+                str(avg_sc), str(var_sc), str(avg_sc_l1), str(var_sc_l1)])
 
         br_iters.append(avg_br)
         social_cost.append(avg_sc)
         social_cost_l1.append(avg_sc_l1)
         equi_found.append( len(social_cost_) / NUM_SIMS )
+
+        br_iters_var.append(var_br)
+        social_cost_var.append(var_sc)
+        social_cost_l1_var.append(var_sc_l1)
     
     if to_plot:
         if with_l1 == True:
@@ -218,8 +214,9 @@ def sweep_d(d_vals, with_l1=False, to_plot=True):
             plot(d_vals, "X dimension", "sweep_d", br_iters, social_cost)
 
 def benchmark_n():
+    print("Benchmarking n")
     start = time.time()
-    sweep_n([10,20,30,40], with_l1=True, to_plot=False)
+    sweep_n([10, 20, 30, 40], with_l1=True, to_plot=True)
     end = time.time()
     print(end-start)
 
@@ -230,15 +227,23 @@ def benchmark_d():
     end = time.time()
     print(end-start)
 
-if __name__ == "__main__":
-    #n_vals = np.concatenate([np.arange(10,100,10), np.arange(100,1050,50), np.arange(1000, 11000, 1000)])
-    #sweep_n(n_vals, with_l1=True)
+def benchmark_p():
+    print("Benchmarking p")
+    start = time.time()
+    sweep_p([1.2,3], with_l1=True, to_plot=False)
+    end = time.time()
+    print(end-start)
 
-    #d_vals = np.arange(1, 11, 1)
-    #sweep_d(d_vals, with_l1=True)
+if __name__ == "__main__":
+    n_vals = np.concatenate([np.arange(10,100,10), np.arange(100,1000,100), np.arange(1000, 11000, 1000)])
+    sweep_n(n_vals, with_l1=True)
+
+    d_vals = np.arange(1, 20, 2)
+    sweep_d(d_vals, with_l1=True)
     
     #p_vals = [1.2, 1.4, 1.6, 1.8, 2, 4, 6, 8, 10, 100]
     #sweep_p(p_vals, with_l1=True)
-    
-    benchmark_d()
+   
+
+
 

p_best_response.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
+## Compute the best response under p regression for a single agent
 
 import numpy as np
 from p_regression import loss_fn_p, fit_model, get_ith_projection, get_H_matrix
 
-eps = 0.0001
+
+def to_update(X, curr_Y, true_Y, i, p):
+    """ Compute whether an agent needs to update their value or not. Helps with early termination
+        Very similar to verify_equilibrium
+        TODO: Make verify equilibrium leverage this"""
+    
+    eps = 0.005
+    proj = np.matmul(X, fit_model(X, curr_Y, p=p))
+    n = X.shape[0]
+
+    def in_range(x, y, eps=eps):
+        """ Check if x - eps < y < x+eps """
+        if x-eps <= y <= x+eps:
+            return True
+        return False
+        
+    if in_range(curr_Y[i], 0) and (proj[i]-eps > true_Y[i] or proj[i] + eps > true_Y[i]):
+        return False
+    elif in_range(curr_Y[i], 1) and (proj[i]-eps < true_Y[i] or proj[i] + eps < true_Y[i]):
+        return False
+    elif not in_range(curr_Y[i], 1) and not in_range(curr_Y[i], 0) and in_range(proj[i], true_Y[i]):
+        return False
+    else:
+        return True
 
 def lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, p):
-    """ Compute the best response for agent i, using a smart form of binary searching """
+    """ Compute the best response for agent i, using a form of binary searching
+        0 means no update from the agent
+        1 means an update from the agent
+        TODO: clean up some of the cases
+    """
+    eps = 0.0001
     def binary_search(min_val, max_val, true_val):
         mid = min_val + ((max_val - min_val) / 2)
         mid_report = curr_Y.copy()
@@ -21,48 +50,78 @@ def lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, p):
         else:                       # Need to go lower
             return binary_search(mid, max_val, true_val)
     
-    
+    if to_update(X, curr_Y, true_Y, i, p) == False:
+        return 0, curr_Y[i]
+
     curr_report = curr_Y[i]
+    old_report = curr_report
     curr_val = get_ith_projection(X, curr_beta, i)
     true_val = true_Y[i]
 
+    # possibly redundant
     if abs(curr_val - true_val) < eps:
-        return curr_report
+        return 0, curr_report
         
     report = curr_Y.copy() 
-    if curr_val > true_val:     # pull down
-        #print("Pull Down")
+    if curr_val > true_val:     # pull down the hyperplane
         report[i] = 0
         beta_zero = fit_model(X, report, p=p)
         val = get_ith_projection(X, beta_zero, i)
         if val > true_val:
-            return 0
+            if old_report == 0:
+                return 0, 0
+            else:
+                return 1, 0
         else:
-            return binary_search(0, curr_report, true_val)
-    
-    elif curr_val < true_val:     # pull up
+            ret = binary_search(0, curr_report, true_val)
+            if old_report == ret:
+                return 0, ret
+            else:
+                return 1, ret
+            
+    elif curr_val < true_val:     # pull up the hyperplane
         #print("Pull Up")
         report[i] = 1
         beta_one = fit_model(X, report, p=p)
         val = get_ith_projection(X, beta_one, i)
         if val < true_val:
-            return 1
+            if old_report == 1:
+                return 0, 1
+            else:
+                return 1, 1
         else:
-            return binary_search(curr_report, 1, true_val)
+            ret = binary_search(curr_report, 1, true_val)
+            if old_report == ret:
+                return 0, ret
+            else:
+                return 1, ret
     else:
         assert False, "should not be here"
 
 def l2_agent_best_response(H, curr_Y, true_Y, i):
+    """ The best response for an agent under 2-norm.
+        Note bar{y} = H*tilde(y), where H is the projection matrix
+        Pretty simple, but a bit obsfucated as I wrote it in vectorized
+        form for efficient
+    """
+    
+    eps = 0.0001
     curr_weight = H[i,i]
+    curr_val = curr_Y[i]
     n = H.shape[0]
     total = 0
 
     ith_row = H[i,:].copy()
     ith_row[i] = 0
     br = np.clip((true_Y[i] - np.dot(ith_row, curr_Y)) / curr_weight, 0, 1)
-    return br
+   
+    if (br - curr_val) < eps:
+        return 0, br
+    else:
+        return 1, br
 
 def test_best_response():
+    eps = 0.005
     #### Test 1 - with p=2 and d=1 ####
     X = np.array([[0,1], [0.25, 1], [0.75, 1], [1,1]])
     true_Y = np.array([0, 0.4, 0.6, 1])
@@ -72,8 +131,8 @@ def test_best_response():
     
     # testing best response
     for i in range(4):
-        br_true = l2_agent_best_response(H_mat, curr_Y, true_Y, i)
-        br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, 2)
+        _, br_true = l2_agent_best_response(H_mat, curr_Y, true_Y, i)
+        _, br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, 2)
         assert abs(br_approx - br_true) < eps, "TEST: agent best response 1 - FAILED"
     print("TEST: agent best response 1 - PASSED")
 
@@ -86,8 +145,8 @@ def test_best_response():
     
     # testing best response
     for i in range(4):
-        br_true = l2_agent_best_response(H_mat, curr_Y, true_Y, i)
-        br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, 2)
+        _, br_true = l2_agent_best_response(H_mat, curr_Y, true_Y, i)
+        _, br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, 2)
         assert abs(br_approx - br_true) < eps, "TEST: agent best response 2 - FAILED"
     print("TEST: agent best response 2 - PASSED")
 
@@ -100,8 +159,8 @@ def test_best_response():
     
     # testing best response
     for i in range(4):
-        br_true = l2_agent_best_response(H_mat, curr_Y, true_Y, i)
-        br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, 2)
+        _, br_true = l2_agent_best_response(H_mat, curr_Y, true_Y, i)
+        _, br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, i, 2)
         assert abs(br_approx - br_true) < eps, "TEST: agent best response 3 - FAILED"
     print("TEST: agent best response 3 - PASSED")
 
@@ -109,7 +168,7 @@ def test_best_response():
     X, true_Y = generate_data(d=3)
     curr_Y = true_Y.copy()
     curr_beta = fit_model(X, true_Y, p=4)
-    br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, 1, 4)
+    _, br_approx = lp_agent_best_response(X, curr_Y, true_Y, curr_beta, 1, 4)
     
     curr_Y[1] = br_approx
     br_utl = abs(true_Y[1] - get_ith_projection(X, fit_model(X, curr_Y, p=4), 1))
@@ -120,7 +179,6 @@ def test_best_response():
         test_utl = abs(true_Y[1] - get_ith_projection(X, fit_model(X, test_Y, p=4), 1))
         assert test_utl > br_utl, "TEST: agent best response 3 - FAILED"
     print("TEST: agent best response 3 - PASSED")
-
 
 if __name__ == "__main__":
     test_best_response()

p_regression.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# Generate data for simulations and code to fit the regression lines for a given p
 
 import cvxpy as cp
 import numpy as np
@@ -7,6 +8,7 @@ import scipy.optimize
 import statsmodels.api as sm
 
 def l1_regression_scipy(X, Y):
+    """ L1 regression using scipy - unstable"""
     def fit(X, beta):
         return np.dot(X, beta)
 
@@ -20,28 +22,63 @@ def l1_regression_scipy(X, Y):
     return beta_l1
 
 def l1_regression(X, Y):
+    """ L1 regression using stats packagage - stable and use this"""
     import warnings
     warnings.filterwarnings("ignore")
     results = sm.QuantReg(Y, X).fit(q=0.5)
     return results.params
 
 def loss_fn_p(X, Y, beta, p_):
+    """ Lp loss function - used for cxvpy to compute the optimal regression params"""
     return cp.pnorm(cp.matmul(X, beta) - Y, p=p_)**p_
 
-def generate_data(d=1, n=4, sigma=0.5):
+def generate_data_naive(d=1, n=4, sigma=0.5):
     """ d is the dimension of the x point
         n is the number of points
         sigma is the standard deviation
+        
+        Need to ensure all points have y vals in [0,1], but don't wanna clip too much
+        SUFFERS FROM ~40% CLIPPING
+        VERY BAD AND DON'T USE
     """
     X = np.random.randn(n, d+1);
     X[:,-1] = np.ones(n)
 
     mean = np.random.uniform(0,1,1)[0]
     true_Y = np.random.normal(mean, sigma, n)
+    before = true_Y
     true_Y = np.clip(true_Y, 0, 1)
     return X, true_Y
+                
+def generate_data(d=1, n=4, sigma=0.5):
+    """ d is the dimension of the x point
+        n is the number of points
+        sigma is the standard deviation
+
+        Generate the X values according to Gaussian distribution centered at 0
+        Generate the beta values according to uniform distribution [0,1]
+        Get the corresponding projections and then add noise
+        Then normalize (not clip!!) to ensure values are between 0 and 1
+        USE THIS - VERY GOOD
+    """
+    X = sigma*np.random.randn(n, d);
+    X = np.concatenate([X, np.ones((n,1))], axis=1)
+    
+    beta_vec = np.concatenate([np.random.uniform(-1,1,d), np.random.uniform(-1,1,1)])
+    noise_vec = (sigma/2)*np.random.randn(n)
+    true_Y = np.matmul(X, beta_vec) + noise_vec
+
+    # normalize
+    true_Y = true_Y + abs(min(0, min(true_Y)))
+    true_Y = true_Y / max(1.0, max(true_Y))
+
+    # safety (but never really needed)
+    clip_Y = np.clip(true_Y, 0, 1) 
+    return X, clip_Y
 
 def fit_model(X, Y, p=2, test=False):
+    """ Find the optimal regression paramter for the input and a choice of p
+    for p != 2, use convex optimization lib - otherwise, use standard least squares"""
     def fit_model_2(X, Y):
         beta_mse = np.linalg.lstsq(X, Y, rcond=None)[0]
         return beta_mse
@@ -60,11 +97,15 @@ def fit_model(X, Y, p=2, test=False):
 
 def get_H_matrix(X):
     """ Get the projection matrix for L2 regression """
-    H_mat = np.matmul(X, np.matmul(np.linalg.inv(np.matmul(X.transpose(), X)), \
-            X.transpose()))
-    return H_mat
+    if X.shape[0] == X.shape[1] - 1:
+        return np.eye(X.shape[0])
+    else:
+        H_mat = np.matmul(X, np.matmul(np.linalg.inv(np.matmul(X.transpose(), X)), \
+                X.transpose()))
+        return H_mat
 
 def get_ith_projection(X, beta, i):
+    """ Get the ith element of the projection vector"""
     y_bar = np.matmul(X, beta)
     return y_bar[i]
 
@@ -117,12 +158,24 @@ def test():
      
     beta_1 = fit_model(X, true_Y, p=1)
     y_bar_p = np.matmul(X, beta_1)
-    print(y_bar_p)
     true_Y_bar = [0, 0.25, 0.75, 1]
     delta = np.sum(np.power(true_Y_bar - y_bar_p, 2))
     assert delta < eps, "TEST: p regression fitting 4 - FAILED"
     print("TEST: p regression fitting 4 - PASSED")
 
-
+    #### Test 5 - L2 regression where d=n ####
+    X, Y = generate_data(d=5, n=5, sigma=0.5)
+    beta = fit_model(X, Y, p=1.2)
+    y_fit = np.matmul(X, beta)
+    delta = np.sum(np.power(y_fit - Y, 2))
+    assert delta < eps, "TEST: p regression fitting 5 - FAILED"
+    print("TEST: p regression fitting 5 - PASSED")
+    
 if __name__ == "__main__":
     test()
+
+
+
+
+
+